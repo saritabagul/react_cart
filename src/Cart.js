7f66898de9e3ef62415f83cdff7e6bc0904fa730
sarita bagul <saritabagul06@gmail.com>
2023-07-28 00:04:56 +0530

    adding proper to cart items

--- src/Cart.js
+++ src/Cart.js
@@ -2,14 +2,48 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            products :[
+                {
+                    price:100,
+                    title:'Watch',
+                    qty:1,
+                    img:'',
+                    id:1
+                },
+                {
+                    price:20000,
+                    title:'phone',
+                    qty:5,
+                    img:'',
+                    id:2
+                },
+                {
+                    price:10000,
+                    title:'Laptop',
+                    qty:4,
+                    img:'',
+                    id:3
+                }
+            ]
+        }
+    }
+
     render(){
-        const arr = [1,2,3,4,5];
+        const { products } = this.state;
         return(
             <div className="cart">
                {
-                  arr.map((item)=>{
-                    return item+5;
-                  })
+                //   arr.map((item)=>{
+                //     return item+5;
+                //   })
+
+                products.map((product) =>{
+                    return <CartItem product={product} key={product.id} />
+                })
                }
             </div>
         );
--- src/CartItem.js
+++ src/CartItem.js
@@ -58,7 +58,8 @@ class CartItem extends React.Component{
        }
 
     render(){
-        const {price,title,qty} = this.state;
+        console.log(this.props);
+        const {price,title,qty} = this.props.product;
         return(
         <div className="cart-item">
             <div className='left-block'>
